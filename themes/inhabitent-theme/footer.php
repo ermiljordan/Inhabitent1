a1e08f663707ee336df3c57e36090f951804ec8e
ermiljordan <ermiljordan64@gmail.com>
2016-11-06 22:12:14 -0800

    first commit

---
+++ themes/inhabitent-theme/footer.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package Inhabitent_Theme
+ */
+
+?>
+
+			</div><!-- #content -->
+
+			<footer id="colophon" class="site-footer" role="contentinfo">
+				<div class="site-info">
+					<div class ="footer-left">
+						<div class ="contact-us">
+							<h3>Contact Us</h3>
+								<p><i class ="fa fa-envelope" aria-hidden="true"></i> info@inhabitent.com</p>
+								<p><i class="fa fa-phone" aria-hidden="true"></i> 778-456-7891</p>
+								<p><i class="fa fa-facebook-square" aria-hidden="true"></i> <i class="fa fa-twitter-square" aria-hidden="true"></i> <i class="fa fa-google-plus-square" aria-hidden="true"></i></p>
+						</div>
+					
+						<div class="business-hours">
+							<h3>Business Hours</h3>
+								<p>Monday - Friday: 9am to 5pm</p>
+								<p>Saturday: 10am to 2pm</p>
+								<p>Sunday: Closed</p>
+						</div>
+				
+						<div class ="footer-logo">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="logo">
+						<img src="<?php echo get_template_directory_uri() . '/images/logos/inhabitent-logo-text.svg'; ?>" class="logo" alt="Inhabitent logo" />
+					</a>
+					</div>
+					</div>
+			
+				<div class="footer-botoom">
+					<p><span>copyright</span> &#169 Inhabitent</p>
+				</div>
+				</div><!-- .site-info -->
+			</footer><!-- #colophon -->
+		</div><!-- #page -->
+
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
---
+++ themes/inhabitent-theme/front-page.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+  <section class ="hero-images">
+    <img src="<?php echo get_template_directory_uri() . '/images/logos/inhabitent-logo-full.svg'; ?>" class ="hero-img" alt="inhabitent circle logo" />
+  </section> 
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+      <section class ="shots-stuff"></section>
+
+      <section class ="inhabitent-journal"></section>
+
+      <section calss ="adventure"> 
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
