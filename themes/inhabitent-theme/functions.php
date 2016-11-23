cfb34a59107ef658f1cfc64837a6417f20c95c8a
ermiljordan <ermiljordan64@gmail.com>
2016-11-23 10:13:39 -0800

    fix findus

--- themes/inhabitent-theme/functions.php
+++ themes/inhabitent-theme/functions.php
@@ -86,7 +86,7 @@ function red_starter_scripts() {
 	wp_enqueue_style( 'red-starter-style', get_stylesheet_uri() );
 	
 	wp_enqueue_script( 'jquery' );
-  wp_enqueue_script ( 'nav-searchbar', get_template_directory_uri() . '/js/scripts.js', array ( 'jquery' ), false, true);\
+  wp_enqueue_script ( 'nav-searchbar', get_template_directory_uri() . '/js/script.js', array ( 'jquery' ), false, true);\
 
 	wp_enqueue_script( 'red-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20130115', true );
 	wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css');
