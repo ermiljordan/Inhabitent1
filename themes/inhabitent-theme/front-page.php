a34b0ee3e64023b159677fbc315f016e6a5ea535
ermiljordan <ermiljordan64@gmail.com>
2016-11-22 20:14:48 -0800

    fix product page

--- themes/inhabitent-theme/front-page.php
+++ themes/inhabitent-theme/front-page.php
@@ -39,7 +39,7 @@ get_header(); ?>
           <?php echo $term->description ?>
         </p>
 
-  <p><a href="<?php echo $url ?>" class="shop-botton"><?php echo $term->name.' Stuff'; ?></a></p>
+<p><a href="<?php echo $url ?>" class="shop-botton"><?php echo $term->name.' Stuff'; ?></a></p>
     </div>
         <?php
         }
--- themes/inhabitent-theme/single.php
+++ themes/inhabitent-theme/single.php
@@ -4,7 +4,6 @@
  *
  * @package RED_Starter_Theme
  */
-
 get_header(); ?>
 
 	<div id="primary" class="content-area">
@@ -13,20 +12,24 @@ get_header(); ?>
 		<?php while ( have_posts() ) : the_post(); ?>
 
 			<?php get_template_part( 'template-parts/content', 'single' ); ?>
-
-			<?php the_post_navigation(); ?>
+            <div class= "social-buttons">
+                <button class = "like-button" href ="#"><i class="fa fa-facebook" aria-hidden="true"></i> Like</a>
+                <button class = "twitter-button" href ="#"><i class="fa fa-twitter" aria-hidden="true"></i> Tweet</a>
+                <button class = "pinterest-button" href ="#"><i class="fa fa-pinterest" aria-hidden="true"></i> Pin</a>
+			</div>
+            <?php the_post_navigation(); ?>
 
 			<?php
-
+				// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) :
 					comments_template();
 				endif;
 			?>
 
-		<?php endwhile; ?>
+		<?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
+        <?php get_sidebar(); ?>
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
